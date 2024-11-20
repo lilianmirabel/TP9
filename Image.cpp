396abb2ab6972b8549393373546f6fe6d32c4ad4
lilian <lilianmirabel01120@gmail.com>
2024-11-20 13:35:01 +0100

    First Commit

---
+++ Image.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lilia on 20/11/2024.
+//
+
+#include "Image.h"
---
+++ Image.h
@@ -0,0 +1,16 @@
+//
+// Created by lilia on 20/11/2024.
+//
+
+#ifndef IMAGE_H
+#define IMAGE_H
+
+
+
+class Image {
+
+};
+
+
+
+#endif //IMAGE_H
